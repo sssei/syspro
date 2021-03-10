f7950daf64cf89d5df2441cefbe0dd795376b4fd
Seiga Ueno <seiga.ueno@gmail.com>
2021-03-10 19:20:33 +0900

    complete report2

---
+++ report2/kadai-d/string.c
@@ -0,0 +1,43 @@
+#include "string.h"
+
+size_t str_len(const char* str){
+  size_t len;
+  for(len = 0; str[len] != '\0'; len++);
+  return len;
+}
+
+void str_join(char* str1, const char* str2){
+  int i;
+  for(i = 0; i <= str_len(str2); i++){
+    str1[str_len(str1) + i] = str2[i];
+  }
+
+  return;
+}
+
+int int2char(char *buf, int num){
+  int num_size = 0;
+  int i;
+  int n = num;
+  int mod = 1;
+  if(num == 0){
+    buf[0] = '0';
+    return 1;
+  }
+  while(n){
+    n /= 10;
+    num_size++;
+    mod *= 10;
+  }
+  mod /= 10;
+  for(i = 0; i < num_size; i++){
+    buf[i] = (num / mod) % 10 + '0';
+    mod /= 10;
+  }
+  buf[i] = '\0';
+  return num_size;
+}
+
+
+
+
---
+++ report2/kadai-d/string.h
@@ -0,0 +1,10 @@
+#ifndef _STRING_H_
+#define _STRING_H_
+#include <stdio.h>
+#include <stdlib.h>
+
+size_t str_len(const char *);
+void str_join(char*, const char*);
+int int2char(char*, int);
+
+#endif
