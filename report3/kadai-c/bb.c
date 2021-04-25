5135a031bbe713dbfa4737592f56000c2bd8fd0e
Seiga Ueno <seiga.ueno@gmail.com>
2021-04-25 21:47:20 +0900

    WIP:week3

---
+++ report3/kadai-c/bb.c
@@ -0,0 +1,30 @@
+#include "bb.h"
+#define BUFF_SIZE 10
+
+int bb_buf[BUFF_SIZE];
+int buf_in = 0;
+int buf_out = 0;
+
+int bb_get(void){
+  pthread_mutex_lock(&mutex);
+  while(buf_in == buf_out){
+    pthread_cond_wait(&cond_in, &mutex);    
+  }
+  int res = bb_buf[buf_out];
+  buf_out = (buf_out + 1) % BUFF_SIZE;
+  pthread_cond_signal(&cond_out);
+  pthread_mutex_unlock(&mutex);
+  return res;
+}
+
+void bb_put(int i){
+  pthread_mutex_lock(&mutex);
+  while(buf_in == (buf_out + 1) % BUFF_SIZE){
+    pthread_cond_wait(&cond_out, &mutex);
+  }
+  bb_buf[buf_in] = i;
+  buf_in = (buf_in + 1) % BUFF_SIZE;
+  pthread_cond_signal(&cond_in);
+  pthread_mutex_unlock(&mutex);
+  return;
+}
