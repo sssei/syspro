b9761e1cfaa1038406eaafb29428b40903976fe5
Seiga Ueno <seiga.ueno@gmail.com>
2021-05-24 08:58:17 +0900

    WIP:tcpechoserver2

--- report4/kadai-b/tcpechoserver2.c
+++ report4/kadai-b/tcpechoserver2.c
@@ -7,19 +7,18 @@
 #include <string.h>
 #include <sys/epoll.h>
 
-#define MAXLINE 1024
-#define MAX_EVENTS 10
+#define BUFSIZE 1024
+#define NEVENTS 16
 #define handle_error(msg) \
     do {perror(msg); exit(EXIT_FAILURE);} while (0)
 
 int main(int argc, char const *argv[]){
-  int server_fd, port, i, nfds, valsize;
-  int epoll_fd, new_socket;
-  char buffer[MAXLINE];  
-  struct sockaddr_in address;
-  int addrlen = sizeof(address);
-  struct epoll_event ev, events[MAX_EVENTS];  
-
+  int server_fd, port, i, nfds, sock;
+  int epoll_fd, n;
+  socklen_t len;
+  struct sockaddr_in address, client;
+  struct epoll_event ev, ev_ret[NEVENTS];
+  
   if(argc != 2){
     fwrite("operand error\n", 1, 14, stderr);
     return 1;
@@ -46,31 +45,7 @@ int main(int argc, char const *argv[]){
   if((epoll_fd = epoll_create1(0)) == -1){
     handle_error("epoll_create1");
   }
+  
 
-  ev.events = EPOLLIN;
-  ev.data.fd = server_fd;
-  if(epoll_ctl(epoll_fd, EPOLL_CTL_ADD, server_fd, &ev) == -1){
-    handle_error("epoll_ctrl");
-  }
-  
-  while(1){
-    nfds = epoll_wait(epoll_fd, events, MAX_EVENTS, -1);
-    if(nfds == -1){
-      handle_error("epoll_wait");
-    }
-    for(i = 0; i < nfds; i++){
-      if(events[i].data.fd == server_fd){
-	if((new_socket = accept(server_fd, (struct sockaddr *)&address,
-				(socklen_t*)&addrlen)) < 0){
-	  handle_error("accept");
-	}
-	bzero(buffer, MAXLINE);
-	valsize = read( new_socket , buffer, MAXLINE);
-	buffer[valsize] = '\0';
-	send(new_socket, buffer, strlen(buffer), 0);
-      }
-    }
-  }
-  
   return 0;
 }
