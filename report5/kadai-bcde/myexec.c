0a36143c88903818ae7d271b5b78726c3caac3b7
Seiga Ueno <seiga.ueno@gmail.com>
2021-06-12 12:26:09 +0900

    kadai-d

--- report5/kadai-bcde/myexec.c
+++ report5/kadai-bcde/myexec.c
@@ -1,3 +1,4 @@
+#define _GNU_SOURCE // execvpe 
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
@@ -5,48 +6,54 @@
 #include <fcntl.h>
 #include "parse.h"
 
-static void execute_process(process* curr_proc, int read_fd, int write_fd, char* envp[]){
-    pid_t pid;
-    int status;
+#define handle_error(msg) do { perror(msg); exit(EXIT_FAILURE); } while (0)
 
-    if((pid = fork()) == 0){
-	if(read_fd > 0) dup2(read_fd, 0);
-	if(write_fd > 0) dup2(write_fd, 1);
-	if(curr_proc->input_redirection){
-	    int fd = open(curr_proc->input_redirection, O_RDONLY);
-	    dup2(fd, 0);
-	}
-	if(curr_proc->output_redirection){
-	    int fd = open(curr_proc->output_redirection, O_WRONLY);
-	    dup2(fd, 1);
-	}
-	execve(curr_proc->program_name, curr_proc->argument_list, envp);
-    }else{
-	waitpid(pid, &status, WUNTRACED);
-    }
-
-    return;
-}
-    
 void execute_job(job* curr_job, char* envp[]){
-    process *curr_proc, *next_proc;
-    curr_proc = curr_job->process_list;
-    next_proc = curr_proc->next;
-    int fd[2];
-    int ret;
+  process *curr_proc;
+  curr_proc = curr_job->process_list;
+  int fd[2];
+  int pids[LINELEN];
+  int i, p_cnt = 0;
+  int fdin = STDIN_FILENO;
+  int status, f;
+  pid_t pid;
 
-    if(next_proc == NULL){
-	execute_process(curr_proc, -1, -1, envp);	
-    }else{
-	ret = pipe(fd);
-	if(ret == -1){
-	    perror("pipe");
-	    exit(1);
+  while(curr_proc != NULL){
+    if(pipe(fd) == -1) handle_error("pipe");
+    if((pid = fork()) == -1){
+      handle_error("fork");
+    }else if(pid == 0){
+      dup2(fdin, STDIN_FILENO);
+      if(curr_proc->next != NULL){
+	dup2(fd[1], STDOUT_FILENO);
+      }
+      if(curr_proc->input_redirection){
+	f = open(curr_proc->input_redirection, O_RDONLY);
+	dup2(f, STDIN_FILENO);
+      }
+      if(curr_proc->output_redirection){
+	if(curr_proc->output_option == TRUNC){
+	  f = open(curr_proc->output_redirection, O_WRONLY | O_CREAT | O_TRUNC, 0666);
+	}else{
+	  f = open(curr_proc->output_redirection, O_WRONLY | O_CREAT | O_APPEND, 0666);
 	}
-	execute_process(curr_proc, -1, fd[1], envp);
-	execute_process(curr_proc->next, fd[0], -1, envp);
+	dup2(f, STDOUT_FILENO);
+      }
+      close(fd[0]);
+      execvpe(curr_proc->program_name, curr_proc->argument_list, envp);
+      handle_error("execve");
+    }else{
+      pids[p_cnt] = pid;
+      p_cnt++;
+      close(fd[1]);
+      fdin = fd[0];
+      curr_proc = curr_proc->next;
     }
+  }
 
-
-    return;
+  for(i = 0; i < p_cnt; i++){
+    waitpid(pids[i], &status, WUNTRACED);
+  }
+  
+  return;
 }
